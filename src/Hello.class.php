17433b2110b368f8c36e245e777db4b920da3ecc
ly <364819377@qq.com>
2018-06-01 11:58:59 +0800

    init

---
+++ src/Hello.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tyrans@qq.com
+ * Date: 2018/6/1
+ * Time: 11:53
+ */
+namespace tyrans;
+
+class Hello {
+    public static function greet()
+    {
+        echo 'hello composer';
+    }
+}
